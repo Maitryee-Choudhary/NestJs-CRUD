b302cb59ccedb25f47e3d1705627a6e07cc04fde
Maitryee-Choudhary <maitryeechudhary0715@gmail.com>
2023-05-25 16:21:34 +0530

    added dockerfile

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -14,7 +14,7 @@ import { JwtStrategy } from './auth.jwt.strategy';
   imports: [UserModule, PassportModule,
     JwtModule.register({
         secret: jwtConstants.secret,
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: '2400s' },
     }),
    ],
   providers: [AuthService, LocalStrategy, JwtStrategy],
